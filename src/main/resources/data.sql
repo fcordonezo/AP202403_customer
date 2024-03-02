4de599c50c519ef5a9a87b459f1ea3a8d27677f6
Fabian Camilo Ordoñez <camilo.ordonezo@pragma.com.co>
2024-03-02 17:42:31 -0500

    feat: Se agrega data inicial de BD

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `customer` (`full_name`, `access_password`, `income`, `city`, `age`) VALUES ('Nombre 1', 'shop-01', 190.1, 'Bogotá', 21);
+INSERT INTO `customer` (`full_name`, `access_password`, `income`, `city`, `age`) VALUES ('Nombre 2', 'shop-02', 190.1, 'Bogotá', 21);
+INSERT INTO `customer` (`full_name`, `access_password`, `income`, `city`, `age`) VALUES ('Nombre 3', 'shop-03', 190.1, 'Bogotá', 21);
+INSERT INTO `customer` (`full_name`, `access_password`, `income`, `city`, `age`) VALUES ('Nombre 4', 'shop-04', 190.1, 'Bogotá', 21);
